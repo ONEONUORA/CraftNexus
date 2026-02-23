ff22a07464c3dc03c8064e4df6642740f9e1f0f4
jahrulezfrancis <longsaarmuknaan@gmail.com>
2026-02-23 18:27:21 +0100

    feat(home): implement Browse Categories section on landing page

--- craft-nexus/app/page.tsx
+++ craft-nexus/app/page.tsx
@@ -1,3 +1,4 @@
+import CategorySection from "@/components/landing/BrowseCategories";
 import HeroSection from "@/components/organisms/HomepageHero";
 
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <div className="">
       <HeroSection />
+      <CategorySection />
     </div>
   );
 }
